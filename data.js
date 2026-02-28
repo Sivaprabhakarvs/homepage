e61ae71dbb863a0b42294241f094a0a8c586bd4c
Siva Prabhakar <sivaprabhakar123@gmail.com>
2026-02-28 20:41:41 +0530

    initial commit

---
+++ data.js
@@ -0,0 +1,100 @@
+/*
+ * Copyright © 2026 Siva Prabhakar (https://sivaprabhakar.in). All Rights Reserved.
+ * Base Design: Copyright © 2026 Jaisal E. K. (https://jaisal.in). Base Design Reuse Rights Reserved.
+ * Design completed and handed over to Siva Prabhakar on 27 February 2026. Provided as a finished project
+ * for independent use and maintenance, without ongoing warranties or liabilities.
+ */
+
+const galleryData = {
+  "artworks": [
+    {
+      "filename": "solo_1.avif",
+      "title": "",
+      "category": "gallery",
+	  "folder": "Solo"
+    },
+    {
+      "filename": "solo_2.avif",
+      "title": "",
+      "category": "gallery",
+	  "folder": "Solo"
+    },
+    {
+      "filename": "friend.avif",
+      "title": "",
+      "category": "gallery",
+	  "folder": "Family"
+    },
+    {
+      "filename": "family.avif",
+      "title": "",
+      "category": "gallery",
+	  "folder": "Family"
+    },
+    {
+      "filename": "vikram.avif",
+      "title": "Vikram",
+      "category": "pencil_sketches"
+    },
+    {
+      "filename": "dashamoolam_damu.avif",
+      "title": "Dashamoolam Damu",
+      "category": "pencil_sketches"
+    },
+    {
+      "filename": "shammi.avif",
+      "title": "Shammi",
+      "category": "pencil_sketches"
+    },
+    {
+      "filename": "cbi.avif",
+      "title": "CBI",
+      "category": "pencil_sketches"
+    },
+    {
+      "filename": "phoenix.avif",
+      "title": "Phoenix",
+      "category": "pencil_sketches"
+    },
+    {
+      "filename": "messi.avif",
+      "title": "Messi",
+      "category": "pencil_sketches"
+    },
+    {
+      "filename": "mount.avif",
+      "title": "Mount",
+      "category": "photographs"
+    },
+    {
+      "filename": "petronas.avif",
+      "title": "Petronas Towers",
+      "category": "photographs"
+    },
+    {
+      "filename": "spread_of_life.avif",
+      "title": "Spread of Life",
+      "category": "photographs"
+    },
+    {
+      "filename": "grab.avif",
+      "title": "Grab",
+      "category": "photographs"
+    },
+    {
+      "filename": "kokku.avif",
+      "title": "Kokku",
+      "category": "photographs"
+    },
+    {
+      "filename": "kotti.avif",
+      "title": "Kotti",
+      "category": "photographs"
+    },
+    {
+      "filename": "peacock.avif",
+      "title": "Peacock",
+      "category": "photographs"
+    }
+  ]
+};
